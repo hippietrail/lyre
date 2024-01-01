94f8fae74cba76a26f601708d019f7a8d1cf6b50
Andrew Dunbar <hippytrail@gmail.com>
2024-01-01 13:18:59 +0700

    channels++

--- commands/yt.js
+++ commands/yt.js
@@ -33,25 +33,40 @@ export const execute2 = ytretro;
 // IDs starting with UU are the playlists for the whole channel
 // and are derived from the channel IDs, which start with UC
 const codingChans = {
-    'Acerola':          'UUQG40havu4kNpB4pxUDQhYQ',
-    'AngeTheGreat':     'UUV0t1y4h_6-2SqEpXBXgwFQ',
-    'Bisqwit':          'UUKTehwyGCKF-b2wo0RKwrcg',
-    'Code Bullet':      'UU0e3QhIYukixgh5VVpKHH9Q',
-    'fasterthanlime':   'UUs4fQRyl1TJvoeOdekW6lYA',
-    'javidx9':          'UU-yuWVUplUJZvieEligKBkA',
-    'Sebastian Lague':  'UUmtyQOKKmrMVaKuRXz02jbQ',
-    'suckerpinch':      'UU3azLjQuz9s5qk76KEXaTvA',
-    'The Coding Train': 'UUvjgXvBlbQiydffZU7m1_aw',
-    'Tsoding Daily':    'UUrqM0Ym_NbK1fqeQG2VIohg',
+    'Acerola':                      'UUQG40havu4kNpB4pxUDQhYQ',
+    'AngeTheGreat':                 'UUV0t1y4h_6-2SqEpXBXgwFQ',
+    'AppleProgramming':             'UUDg-YmnNehm3KB0BpytkUJg',
+    'Bisqwit':                      'UUKTehwyGCKF-b2wo0RKwrcg',
+    'ChibiAkumas':                  'UU8t99gp5IN-FTf5rGVaRevw',
+    'Code Bullet':                  'UU0e3QhIYukixgh5VVpKHH9Q',
+    'fasterthanlime':               'UUs4fQRyl1TJvoeOdekW6lYA',
+    'Inigo Quilez':                 'UUdmAhiG8HQDlz8uyekw4ENw',
+    'javidx9':                      'UU-yuWVUplUJZvieEligKBkA',
+    'Sebastian Lague':              'UUmtyQOKKmrMVaKuRXz02jbQ',
+    'StatQuest with Josh Starmer':  'UUtYLUTtgS3k1Fg4y5tAhLbw',
+    'suckerpinch':                  'UU3azLjQuz9s5qk76KEXaTvA',
+    'The Art of Code':              'UUcAlTqd9zID6aNX3TzwxJXg',
+    'The Coding Train':             'UUvjgXvBlbQiydffZU7m1_aw',
+    'Tsoding Daily':                'UUrqM0Ym_NbK1fqeQG2VIohg',
 };
 
 const retroChans = {
     'Adrian\'s Digital Basement':       'UUE5dIscvDxrb7CD5uiJJOiw',
     'Adrian\'s Digital Basement ][':    'UUbtwi4wK1YXd9AyV_4UcE6g',
+    'ChibiAkumas':                      'UU8t99gp5IN-FTf5rGVaRevw',
+    'Jan Beta':                         'UUftUpOO4h9EgH0eDOZtjzcA',
+    'LGR':                              'UULx053rWZxCiYWsBETgdKrQ',
+    'Modern Vintage Gamer':             'UUjFaPUcJU1vwk193mnW_w1w',
     'Noel\'s Retro Lab':                'UU2-SP1bYi3ueKlVU7I75wFw',
+    'Nostalgia Nerd':                   'UU7qPftDWPw9XuExpSgfkmJQ',
+    'RetroVirtualMachine':              'UUgNfOsqL76T13tUex62gonA',
     'RMC - The Cave':                   'UULEoyoOKZK0idGqSc6Pi23w',
+    'Tech Tangents':                    'UUerEIdrEW-IqwvlH8lTQUJQ',
     'The 8-Bit Guy':                    'UU8uT9cgJorJPWu7ITLGo9Ww',
+    'The Byte Attic':                   'UUfzZNuoHys1t-AdwYDhOz8g',
     'The Clueless Engineer':            'UURgWN7MQrH4V3o9wB47DYzA',
+    'The Retro Desk':                   'UUWihlGXWuyJbjP5vjzD03Rw',
+    'Usagi Electric':                   'UUE4xstUnu0YmkG-W9_PyYrQ',
 };
 
 async function ytcoding(interaction) {
