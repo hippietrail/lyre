15d692f4b943d894fd541b2072cddb2c62b80d5c
Andrew Dunbar <hippytrail@gmail.com>
2024-01-05 21:59:06 +0700

    add a version of fetchJson with error logging for when sites are not working properly

--- ute/earl.js
+++ ute/earl.js
@@ -20,4 +20,19 @@ export class Earl {
     async fetchJson() {
         return (await fetch(this.url)).json();
     }
+
+    // apilayer.com that we use for currency exchange rates is sometimes broken
+    async fetchJsonWithError() {
+        const r = await fetch(this.url);
+        if (r.headers.get('content-type') !== 'application/json') {
+            console.log(typeof r);
+            console.log(r);
+            console.log(Object.keys(r));
+            // how can we access files of an object which are Symbols?
+            console.log(Object.getOwnPropertySymbols(r));
+            console.log(JSON.stringify(r, null, 2));
+            return null;
+        }
+        return await r.json();
+    }
 }
