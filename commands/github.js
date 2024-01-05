59d85d363aae1e19ba6a92273ff893296bf39dee
Andrew Dunbar <hippytrail@gmail.com>
2024-01-05 21:56:52 +0700

    go full functional with github.js!

--- commands/github.js
+++ commands/github.js
@@ -34,33 +34,31 @@ export const execute = async interaction => {
 
         if (usernames.length === 0) {
             await interaction.editReply('No GitHub usernames specified or in the .env file');
-            return;
+        } else {
+            const now = new Date();
+
+            const reply = (await Promise.all(usernames.map(async user => {
+                githubEarl.setUserName(user);
+
+                return (await githubEarl.fetchJson())
+                    .slice(0, 3)
+                    .map(e => ({
+                        user: e.actor.login,
+                        type: e.type.split(/(?=[A-Z])/).slice(0, -1).join(' ').toLowerCase(),
+                        payloadAction: e.payload.action,
+                        repo: e.repo.name,
+                        created_at: e.created_at,
+                        elapsed_time: now - new Date(e.created_at),
+                    }));
+            })))
+                .flat()
+                .sort((a, b) => a.elapsed_time - b.elapsed_time)
+                .map(e => `${e.user}: ${e.type}${e.payloadAction ? `.${e.payloadAction}` : ''
+                    } ${e.repo} - ${ago(e.elapsed_time)}`)
+                .join('\n');
+
+            await interaction.editReply(reply !== "" ? reply : 'No events found');
         }
-        const now = new Date();
-
-        const promises = usernames.map(async user => {
-            githubEarl.setUserName(user);
-            const events = (await githubEarl.fetchJson()).slice(0, 3);
-
-            return events.map(e => ({
-                user: e.actor.login,
-                type: e.type.split(/(?=[A-Z])/).slice(0, -1).join(' ').toLowerCase(),
-                payloadAction: e.payload.action,
-                repo: e.repo.name,
-                created_at: e.created_at,
-                elapsed_time: now - new Date(e.created_at),
-            }));
-        });
-
-        const replies = await Promise.all(promises);
-        const events = replies.flat();
-        const sortedEvents = events.sort((a, b) => a.elapsed_time - b.elapsed_time);
-
-        const reply = sortedEvents.map(e => `${e.user}: ${e.type}${
-            e.payloadAction ? `.${e.payloadAction}` : ''
-        } ${e.repo} - ${ago(e.elapsed_time)}`).join('\n');
-
-        await interaction.editReply(reply !== "" ? reply : 'No events found');
     } catch (error) {
         console.error(error);
         await interaction.editReply('An error occurred while fetching data.');
