edb6588507ba13078cc8ac563e5c0a0245853f85
Andrew Dunbar <hippytrail@gmail.com>
2024-01-09 01:58:40 +0700

    better etymonline scraping and parsing

--- commands/etym.js
+++ commands/etym.js
@@ -1,4 +1,3 @@
-// a new slash command that checks if a word in in Etymonline
 import { SlashCommandBuilder } from 'discord.js';
 import { Earl } from '../ute/earl.js';
 import parse from 'html-dom-parser';
@@ -14,120 +13,67 @@ async function etym(interaction) {
     await interaction.deferReply();
     try {
         const earl = new Earl('https://etymonline.com', '/word/');
-        earl.setLastPathSegment(interaction.options.getString('word'));
-        const dommy = parse(await earl.fetchText());
+        const word = interaction.options.getString('word');
+        earl.setLastPathSegment(word);
+        const dom = parse(await earl.fetchText());
 
-        const whatWeFound = lookForClass(dommy, 'ant-col-lg-17', 0);
+        const html = dom[3];
+        if (!html || html.type !== 'tag' || html.name !== 'html')
+            throw new Error('html not found');
+        const body = html.children[3];
+        if (!body || body.type !== 'tag' || body.name !== 'body')
+            throw new Error('body not found');
+        const root = body.children[1];
+        if (!root || root.type !== 'tag' || root.name !== 'div' || root.attribs?.id !== 'root')
+            throw new Error('root not found');
+        const reactRoot = root.children[0];
+        if (!reactRoot || reactRoot.type !== 'tag' || reactRoot.name !== 'div' || !('data-reactroot' in reactRoot.attribs))
+            throw new Error('reactRoot not found');
+        const container1mazc = reactRoot.children[0];
+        if (!container1mazc || container1mazc.type !== 'tag' || container1mazc.name !== 'div' || !container1mazc.attribs?.class?.includes('container--1mazc'))
+            throw new Error('container1mazc not found');
+        const main = container1mazc.children[1];
+        if (!main || main.type !== 'tag' || main.name !== 'div' || !('data-role' in main.attribs) || main.attribs['data-role'] !== 'content-main')
+            throw new Error('contnet-main not found');
+        const antRow = main.children[0];
+        if (!antRow || antRow.type !== 'tag' || antRow.name !== 'div' || !antRow.attribs?.class?.startsWith('ant-row'))
+            throw new Error('ant-row not found');
+        const lg17 = antRow.children[0];
+        if (!lg17 || lg17.type !== 'tag' || lg17.name !== 'div' || !lg17.attribs?.class?.includes('ant-col-lg-17'))
+            throw new Error('ant-col-lg-17 not found');
 
-        console.log(`lookForClass: ${JSON.stringify(whatWeFound, null, 2)}`);
+        // if the children are [h2, p, p] then the word is not in etymonline
+        if (lg17.children[0].name === 'h2' && lg17.children[1].name === 'p' && lg17.children[2].name === 'p') {
+            await interaction.editReply('Not in Etymonline');
+        } else {
+            const wordNodes = lg17.children.filter(node => node.name === 'div' && node.attribs?.class?.includes('word--C9UPa'));
 
-        let reply = '???';
-        switch (whatWeFound) {
-            case 404:
-                reply = "Not found";
-                break;
-            case 'FOUND':
-                reply = "Found";
-                break;
-            case 'FOUND2':
-                reply = "Found (2)";
-                break;
+            let reply = `[there are ${wordNodes.length} word sections.](${earl.getUrlString()})`;
+
+            const words = [...new Set(wordNodes
+                .map(node => node.children[0].children[0])
+                .map(node => node.children.find(child => child.attribs?.class?.includes('word__name--TTbAA')))
+                .map(node => node.children[0].data))];
+
+            // is at least one of the words a case-insensitive match?
+            if (words.some(w => w.toLowerCase() === word.toLowerCase())) {
+                if (words.length === 1) {
+                    reply += `\n'${words[0]}' matches '${word}.`;
+                } else {
+                    reply += `\nat least one of ${words.map(node => `'${node}'`).join(', ')} matches '${word}.`;
+                }
+            } else {
+                if (words.length === 1) {
+                    reply += `\nbut '${words[0]}' does not match '${word}'!`;
+                } else {
+                    reply += `\nbut none of ${words.map(node => `'${node}'`).join(', ')} match '${word}'!`;
+                }
+            }
+
+            await interaction.editReply(reply);
         }
-        await interaction.editReply(`${reply}: ${earl.getUrlString()}`);
     } catch (e) {
         console.error(e);
         await interaction.editReply('An error occurred while fetching data.');
     }
-}
-
-function exploreEtymonlineDom(stuffs, startDepth, maxDepth) {
-    if (maxDepth === undefined) maxDepth = 8;
-
-    for (const [index, n] of stuffs.entries()) {
-        printNode(n, startDepth, index);
-        if (
-            'children' in n &&
-            'name' in n &&
-            ['div', 'html', 'body'].includes(n.name) &&
-            startDepth < maxDepth
-        ) {
-            exploreEtymonlineDom(n.children, startDepth + 1, maxDepth);
-        }
-    }
-}
-  
-function lookForClass(stuffs, className, depth) {
-    const patterns = [
-        [404, ['h2', 'p', 'p']],
-        ['FOUND', ['div', 'div', 'div', 'div', 'div', 'div', 'div', 'div', 'p', 'div']],
-        ['FOUND2', ['div', 'div', 'div', 'div', 'div', 'div', 'div', 'div', 'div', 'div', 'p', 'div']],
-    ];
-
-    function cmp(nodesKidsNames, matchNames) {
-        return nodesKidsNames.length === matchNames.length
-            && nodesKidsNames.every((name, i) => matchNames[i] === name);
-    }
-
-    for (const node of stuffs) {
-        if ('children' in node && 'name' in node && ['div', 'html', 'body'].includes(node.name)) {
-            if (node.attribs.class?.includes(className)) {
-                console.log(`Found ${className} at depth ${depth}`);
-                console.log(node.children.map(c => c.name));
-                const kidNodeNames = node.children.map(c => c.name);
-
-                let matched = null;
-                patterns.some((tup, index) => {
-                    if (cmp(kidNodeNames, tup[1])) {
-                        matched = patterns[index];
-                        return true;
-                    }
-                    return false;
-                });
-
-                if (matched !== null) return matched[0];
-
-                console.log('Did not find any known pattern of child nodes here');
-                exploreEtymonlineDom(node.children, depth + 1);
-
-                // remove the child nodes for ad content, they have an 'data-ad-container' attribute
-                console.log(node.children.filter(c => !('data-ad-container' in c.attribs)).map(c => `${c.name} ${c.attribs.class}`));
-            }
-            const result = lookForClass(node.children, className, depth + 1);
-            if (result !== null) return result;
-        }
-    }
-    return null;
-}
-
-function printNode(node, depth, index) {
-    if (node.type === 'text' && node.data.trim() === '') return;
-    if (node.type === 'script') return;
-
-    if (node.type !== 'tag') {
-        console.log(`${
-            ' '.repeat(depth)
-        } ${`[${depth}.${index}] `}type: ${
-            node.type
-        }${
-            node.name ? `, name: ${node.name}` : ''
-        } [ ${Object.keys(node).join(', ')} ]`
-        );
-    } else {
-        console.log(`${' '.repeat(depth)} ${`[${depth}.${index}] `}<${node.name}>`);
-    }
-
-    if ('attribs' in node && Object.keys(node.attribs).length > 0) {
-        if (node.attribs.id)
-            console.log(`${' '.repeat(depth+1+6)}#`, node.attribs.id);
-        if (node.attribs.class)
-            console.log(`${' '.repeat(depth+1+5)}`, node.attribs.class.split(' ').map((c) => `.${c}`));
-
-        // if there are attribs/attributes other than 'id' and/or 'class', we can print them
-        const others = Object.keys(node.attribs).filter((key) => !['id', 'class'].includes(key));
-        if (others.length > 0)
-            console.log(`${' '.repeat(depth+1+6)}@@`, others.map((key) => `${key}: ${node.attribs[key]}`).join(', '));
-    }
-
-    if (node.type === 'text')
-        console.log(`${' '.repeat(depth+1)}'"""'${node.data.trim()}'"""'`);
-}
+}  
