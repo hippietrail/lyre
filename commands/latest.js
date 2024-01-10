811d588e5d80bf5adf9d7cd8163b8e698d36199a
Andrew Dunbar <hippytrail@gmail.com>
2024-01-10 18:35:36 +0700

    refactor goXyz functs to return arrays instead of strings, now prints them in order

--- commands/latest.js
+++ commands/latest.js
@@ -48,7 +48,7 @@ async function latest(interaction) {
     await interaction.deferReply();
     let reply = 'An error occurred while fetching data.';
     try {
-        await interaction.editReply('Being gentle on the Github API...', { ephemeral: true });
+        await interaction.editReply('Being gentle on the Github API...');
         const replies = await Promise.all([
             callGithub(),
             callNodejs(),
@@ -57,7 +57,7 @@ async function latest(interaction) {
             callPython(),
             callGo(),
         ]);
-        reply = replies.join('\n');
+        reply = replies.flat().sort().join('\n');
     } catch (error) {
         console.error(error);
     }
@@ -91,31 +91,22 @@ async function callGithub() {
     for (const repo of githubRepos) {
         githubReleasesEarl.setPathname(`/repos/${repo[0]}/${repo[1]}/releases/latest`);
         const ob = await githubReleasesEarl.fetchJson();
-        //console.log(`callGithub: owner: ${repo[0]}, repo: ${repo[1]}, ${
-        //    'name' in ob ? `ob.name: ${ob.name}` : 'API rate limit!'
-        //}`);
-
         const nvlts = githubJsonToNVLTS(repo, ob);
         const nvltsString = nvlts ? nvltsToString(nvlts) : `${repo[1]}: GitHub Error! (API rate limit?)`;
         result.push(nvltsString);
         if (githubRepos.indexOf(repo) < githubRepos.length - 1)
             await new Promise(resolve => setTimeout(resolve, 4500)); // delay for GitHub API rate limit
     }
-    return result.join('\n');
+    return result;
 }
-
+  
 function transformRepoNameTagVer(repo, ob) {
     const [, repoName, xform] = repo;
     const [obName, obTag] = [ob.name, ob.tag_name];
 
     if (xform) {
-        //console.log(`Transforming repo: ${repoName}, name: ${obName}, tag: ${obTag}, xform: ${xform}`);
-        const xformed = xform(repoName, obName, obTag);
-        //console.log(`xformed: ${JSON.stringify(xformed, null, 2)}`);
-        return xformed;
+        return xform(repoName, obName, obTag);
     } else {
-        //console.log(`*NOT* transforming repo: ${repoName}, name: ${obName}, tag: ${obTag}`);
-        // name and ver already set to '?name?' and '?ver?'
         console.log(`Unrecognized repo: ${repoName}, name: ${obName}, tag: ${obTag}`);
         return ['?name?', '?ver?'];
     }
@@ -163,84 +154,85 @@ async function callNodejs() {
         };
     });
 
-    const reply = nvlts.map(nvlts => nvltsToString(nvlts)).join('\n');
-
-    return reply;
+    return nvlts.map(nvlts => nvltsToString(nvlts));
 }
-
+  
 async function callGimp() {
     const gj = await gimpEarl.fetchJson();
     if ('STABLE' in gj) {
         if (gj.STABLE.length > 0) {
             if ('version' in gj.STABLE[0]) {
-                return nvltsToString({
-                    name: 'Gimp',
-                    ver: gj.STABLE[0].version,
-                    link: undefined,
-                    timestamp: new Date(gj.STABLE[0].date),
-                    src: 'gitlab',
-                });
+                return [
+                    nvltsToString({
+                        name: 'Gimp',
+                        ver: gj.STABLE[0].version,
+                        link: undefined,
+                        timestamp: new Date(gj.STABLE[0].date),
+                        src: 'gitlab',
+                    }),
+                ];
             }
         }
     }
-    return '';
+    return [];
 }
-
+  
 async function callXcode() {
     const xcj = await xcodeEarl.fetchJson();
     const rel = xcj.find(obj => obj.name === 'Xcode' && obj.version.release.release === true);
     if (rel) {
-        return nvltsToString({
-            name: 'Xcode',
-            ver: rel.version.number,
-            link: rel.links.notes.url,
-            timestamp: new Date(rel.date.year, rel.date.month - 1, rel.date.day),
-            src: 'xcodereleases.com',
-        });
+        const timestamp = new Date(rel.date.year, rel.date.month - 1, rel.date.day);
+        return [
+            nvltsToString({
+                name: 'Xcode',
+                ver: rel.version.number,
+                link: rel.links.notes.url,
+                timestamp,
+                src: 'xcodereleases.com',
+            }),
+            nvltsToString({
+                name: 'Swift',
+                ver: rel.compilers.swift[0].number,
+                link: rel.links.notes.url,
+                timestamp,
+                src: 'xcodereleases.com',
+            }),
+        ];
     }
-    return '';
+    return [];
 }
-
+  
 async function callPython() {
     pythonEarl.setPathname('/repos/python/cpython/tags');
     const pya = await pythonEarl.fetchJson();
-    // first, check if we got an error, usually hitting the GitHub rate limit
     if (pya.message && pya.documentation_url) {
         console.log(`GitHub tags API error: 'python'${pya.message} ${pya.documentation_url}`);
-        return '';
+        return [];
     }
-
-    //console.log(`callPython: pya: ${JSON.stringify(pya, null, 2)}`);
-    // pya is an array of objects. each has a 'name' field
-    // we want the first one whose name field is of the form vN.N.N
-    // where N is an integer, aka one or more digits
-    const rel = pya.find(obj => {
-        const match = obj.name.match(/^v(\d+)\.(\d+)\.(\d+)$/);
-        return match !== null;
-    });
-
-    console.log(`callPython: rel: ${JSON.stringify(rel, null, 2)}`);
-
+    const rel = pya.find(obj => obj.name.match(/^v(\d+)\.(\d+)\.(\d+)$/));
     if (rel) {
-        return nvltsToString({
-            name: 'Python',
-            ver: rel.name,
-            link: undefined,
-            timestamp: undefined,
-            src: 'github',
-        });
+        return [
+            nvltsToString({
+                name: 'Python',
+                ver: rel.name,
+                link: undefined,
+                timestamp: undefined,
+                src: 'github',
+            }),
+        ];
     }
-
-    return '';
+    return [];
 }
-
+  
 async function callGo() {
     const goj = await goEarl.fetchJson();
-    return nvltsToString({
-        name: 'Go',
-        ver: goj[0].version.replace(/^go/, ''),
-        link: undefined,
-        timestamp: undefined,
-        src: 'go.dev',
-    });
+    return [
+        nvltsToString({
+            name: 'Go',
+            ver: goj[0].version.replace(/^go/, ''),
+            link: undefined,
+            timestamp: undefined,
+            src: 'go.dev',
+        }),
+    ];
 }
