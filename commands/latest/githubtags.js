91a2494f961349ae1181fb315cf16914a9b43d39
Andrew Dunbar <hippytrail@gmail.com>
2024-01-24 00:02:51 +0700

    handle github api error in /latest github tags

--- commands/latest/githubtags.js
+++ commands/latest/githubtags.js
@@ -22,7 +22,7 @@ export async function callGithubTags() {
         result = result.concat(ob);
 
         if (i < ownerRepos.length - 1)
-            await new Promise(resolve => setTimeout(resolve, 2300)); // delay for GitHub API rate limit
+            await new Promise(resolve => setTimeout(resolve, 4600)); // delay for GitHub API rate limit
     }
 
     return result;
@@ -35,31 +35,35 @@ async function callGithubTagsRepo(name, ownerRepo, regex) {
         const ght = await githubTagsEarl.fetchJson();
 
         if (ght.message && ght.documentation_url) {
-            console.log(`[${name}] GitHub tags API error: '${name}'${ght.message} ${ght.documentation_url}`);
+            console.log(`[${name}] GitHub tags API error: '${name}'${ght.message} ${githubTagsEarl.getUrlString()}`);
         } else {
             const rel = ght.find(obj => obj.name.match(regex));
 
             if (rel) {
                 const json = await (await fetch(rel.commit.url)).json();
 
-                // there is commit.author.date and commit.committer.date...
-                const [authorDate, committerDate] = ["author", "committer"].map(k => new Date(json.commit[k].date));
-                // print which is newer, and by how many seconds/minutes
-                // in the one I checked, the committer is newer by about 15 minutes
-                const [newer, older, diff, date] = committerDate > authorDate
-                    ? ['committer', 'author', committerDate - authorDate, committerDate]
-                    : ['author', 'committer', authorDate - committerDate, authorDate];
+                if (json.message && json.documentation_url) {
+                    console.log(`[${name}] GitHub tags API error: '${name}'${json.message} ${rel.commit.url}`);
+                } else {
+                    // there is commit.author.date and commit.committer.date...
+                    const [authorDate, committerDate] = ["author", "committer"].map(k => new Date(json.commit[k].date));
+                    // print which is newer, and by how many seconds/minutes
+                    // in the one I checked, the committer is newer by about 15 minutes
+                    const [newer, older, diff, date] = committerDate > authorDate
+                        ? ['committer', 'author', committerDate - authorDate, committerDate]
+                        : ['author', 'committer', authorDate - committerDate, authorDate];
 
-                if (diff)
-                    console.log(`[${name}] ${newer} is newer than ${older} by ${ago(diff).replace(' ago', '')} (diff: ${diff})`);
+                    if (diff)
+                        console.log(`[${name}] ${newer} is newer than ${older} by ${ago(diff).replace(' ago', '')} (diff: ${diff})`);
 
-                return [{
-                    name: name,
-                    ver: rel.name,
-                    link: json.html_url,
-                    timestamp: date,
-                    src: 'github',
-                }];
+                    return [{
+                        name: name,
+                        ver: rel.name,
+                        link: json.html_url,
+                        timestamp: date,
+                        src: 'github',
+                    }];
+                }
             }
         }
     } catch (error) {
