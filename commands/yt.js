0d46474be6a8b85f51d8aa58213491a0d7771f63
Andrew Dunbar <hippytrail@gmail.com>
2024-01-26 13:46:56 +0700

    move lists of youtube channels into config.json

--- commands/yt.js
+++ commands/yt.js
@@ -1,6 +1,14 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { YoutubeVidsEarl } from '../ute/earl.js';
 import { ago } from '../ute/ago.js';
+import fs from 'node:fs';
+
+let config;
+try {
+    config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+} catch (err) {
+    console.error(`[YouTube] ${err}`);
+}
 
 const ytEarl = new YoutubeVidsEarl();
 ytEarl.setMaxResults(10);
@@ -52,154 +60,52 @@ export const data7 = new SlashCommandBuilder()
 
 export const execute7 = ytall;
 
-// make a map of my favourite coding youtube channel names to their channel IDs
-// IDs starting with UU are the playlists for the whole channel
-// and are derived from the channel IDs, which start with UC
-const codingChans = {
-    name: 'Coding',
-    list: {
-        'Acerola':                      'UUQG40havu4kNpB4pxUDQhYQ',
-        'AngeTheGreat':                 'UUV0t1y4h_6-2SqEpXBXgwFQ',
-        'AppleProgramming':             'UUDg-YmnNehm3KB0BpytkUJg',
-        'Ben Eater':                    'UUS0N5baNlQWJCUrhCEo8WlA',
-        'Bisqwit':                      'UUKTehwyGCKF-b2wo0RKwrcg',
-        'Code Bullet':                  'UU0e3QhIYukixgh5VVpKHH9Q',
-        'fasterthanlime':               'UUs4fQRyl1TJvoeOdekW6lYA',
-        'Inigo Quilez':                 'UUdmAhiG8HQDlz8uyekw4ENw',
-        'javidx9':                      'UU-yuWVUplUJZvieEligKBkA',
-        'Sebastian Lague':              'UUmtyQOKKmrMVaKuRXz02jbQ',
-        'StatQuest with Josh Starmer':  'UUtYLUTtgS3k1Fg4y5tAhLbw',
-        'suckerpinch':                  'UU3azLjQuz9s5qk76KEXaTvA',
-        'The Art of Code':              'UUcAlTqd9zID6aNX3TzwxJXg',
-        'The Coding Train':             'UUvjgXvBlbQiydffZU7m1_aw',
-        'Tom Marks Talks Code':         'UUknRd0bSUDYJ-_oA2r2S6IA',
-        'Tsoding Daily':                'UUrqM0Ym_NbK1fqeQG2VIohg',
-    }
-};
-
-const codingChans2 = {
-    name: 'Coding (2)',
-    list: {
-        'ChibiAkumas':      'UU8t99gp5IN-FTf5rGVaRevw',
-        'sphaerophoria':    'UUXzL31BCxf8En1KT34gSK6g',
-        'Let\'s Get Rusty': 'UUSp-OaMpsO8K0KkOqyBl7_w',
-        'No Boilerplates':  'UCUMwY9iS8oMyWDYIe6_RmoA',
-        'Paul Hudson':      'UUmJi5RdDLgzvkl3Ly0DRMlQ', // Swift, SwiftUI
-        'Philipp Lackner':  'UUKNTZMRHPLXfqlbdOI7mCkg', // Android, Jetpack Compose, Kotlin
-        'ThePrimeTime':     'UUUyeluBRhGPCW4rPe_UvBZQ',
-        'Zig SHOWTIME':     'UU2EQzAewrC10KCDFSS4j-zA',
-    }
-};
-
-const retroChans = {
-    name: 'Retrocomputing',
-    list: {
-        'Action Retro':                     'UUoL8olX-259lS1N6QPyP4IQ',
-        'Adrian\'s Digital Basement':       'UUE5dIscvDxrb7CD5uiJJOiw',
-        'Adrian\'s Digital Basement ][':    'UUbtwi4wK1YXd9AyV_4UcE6g',
-        'Jan Beta':                         'UUftUpOO4h9EgH0eDOZtjzcA',
-        'Modern Vintage Gamer':             'UUjFaPUcJU1vwk193mnW_w1w',
-        'Noel\'s Retro Lab':                'UU2-SP1bYi3ueKlVU7I75wFw',
-        'Nostalgia Nerd':                   'UU7qPftDWPw9XuExpSgfkmJQ',
-        'PCRetroTech':                      'UUWYne_mhlRE1AiN2ApjmZDA',
-        'Retro Hack Shack':                 'UUN_u47_wJuhh249H9E8sjtw',
-        'RetroVirtualMachine':              'UUgNfOsqL76T13tUex62gonA',
-        'RMC - The Cave':                   'UULEoyoOKZK0idGqSc6Pi23w',
-        'Tech Tangents':                    'UUerEIdrEW-IqwvlH8lTQUJQ',
-        'The 8-Bit Guy':                    'UU8uT9cgJorJPWu7ITLGo9Ww',
-        'The Byte Attic':                   'UUfzZNuoHys1t-AdwYDhOz8g',
-        'The Clueless Engineer':            'UURgWN7MQrH4V3o9wB47DYzA',
-        'The Retro Desk':                   'UUWihlGXWuyJbjP5vjzD03Rw',
-        'Usagi Electric':                   'UUE4xstUnu0YmkG-W9_PyYrQ',
-    }
-};
-
-const retroChans2 = {
-    name: 'Retrocomputing (2)',
-    list: {
-        'Cathode Ray Dude':     'UUXnNibvR_YIdyPs8PZIBoEw',
-        'ChibiAkumas':          'UU8t99gp5IN-FTf5rGVaRevw',
-        'Kim Justice':          'UU9ZWVL1Elyt2cdiQYjxS_1w',
-        'LGR':                  'UULx053rWZxCiYWsBETgdKrQ',
-        'Retro Recipes':        'UU6gARF3ICgaLfs3o2znuqXA',
-        'The Spectrum Show':    'UUDOV38mSrjtoFkNpaPuIjLQ',
-    }
-};
-
-const storiesChans = {
-    name: 'Storytelling',
-    list: {
-        'amglimpse':    'UUv766eFS-V6auawYiqXyU1w',
-        'MrBallen':     'UUtPrkXdtCM5DACLufB9jbsA',
-        'Qxir':         'UUGHDQtN_vzFYJaq_Fx1eikg',
-        'Thoughty2':    'UURlICXvO4XR4HMeEB9JjDlA',
-    }
-};
-
-const otherChans = {
-    name: 'Other',
-    list: {
-        'Barn Find Hunter':     'UULgEVx4mzk3T3mzgbKG54Eg',
-        'Captain Disillusion':  'UUEOXxzW2vU0P-0THehuIIeg',
-        'ColdFusion':           'UU4QZ_LsYcvcq7qOsOhpAX4A',
-        'coldwarmotors':        'UUoculxk4_H8XzjPS_zbYUpQ',
-        'Darknet Diaries':      'UUMIqrmh2lMdzhlCPK5ahsAg',
-        'Jay Leno\'s Garage':   'UUQMELFlXQL38KPm8kM-4Adg',
-        'Mustie1':              'UUcSeeATlWJJbXpOZRYOfaDg',
-        'Trash Theory':         'UUxHcoI9ndIdAihEB7ODTOfQ',
-        'Two Minute Papers':    'UUbfYPyITQ-7l4upoX8nvctg',
-    }
-};
-
 async function ytcoding(interaction) {
-    await yt(interaction, codingChans);
+    await yt(interaction, 'coding', config.coding);
 }
 
 async function ytcoding2(interaction) {
-    await yt(interaction, codingChans2);
+    await yt(interaction, 'coding2', config.coding2);
 }
 
 async function ytretro(interaction) {
-    await yt(interaction, retroChans);
+    await yt(interaction, 'retro', config.retro);
 }
 
 async function ytretro2(interaction) {
-    await yt(interaction, retroChans2);
+    await yt(interaction, 'retro2', config.retro2);
 }
 
 async function ytstories(interaction) {
-    await yt(interaction, storiesChans);
+    await yt(interaction, 'stories', config.stories);
 }
 
 async function ytother(interaction) {
-    await yt(interaction, otherChans);
+    await yt(interaction, 'other', config.other);
 }
 
 async function ytall(interaction) {
-    await yt(interaction, {
-        name: 'All',
-        list: {
-            ...codingChans.list,
-            ...codingChans2.list,
-            ...retroChans.list,
-            ...retroChans2.list,
-            ...storiesChans.list,
-            ...otherChans.list,
-        }
-    })
+    await yt(interaction, 'all', {
+        ...config.coding,
+        ...config.coding2,
+        ...config.retro,
+        ...config.retro2,
+        ...config.stories,
+        ...config.other
+    });
 }
 
-async function yt(interaction, chanGroup) {
+async function yt(interaction, chanGroupName, chanList) {
     await interaction.deferReply();
     try {
         const now = new Date();
 
-        if (Object.keys(chanGroup.list).length === 0) {
-            await interaction.editReply(`No channels in group '${chanGroup.name}'`);
+        if (chanList.length === 0) {
+            await interaction.editReply(`No channels in group '${chanGroupName}'`);
             return;
         }
 
-        const allVids = (await Promise.all(Object.values(chanGroup.list).map(
+        const allVids = (await Promise.all(Object.values(chanList).map(
             async plid => await fetchVideos(plid)
         ))).map(chanVids => chanVids.items).flat();
         
