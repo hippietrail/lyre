d2d221a048ed239578794f9be82d501e95e03019
Andrew Dunbar <hippytrail@gmail.com>
2024-02-03 02:20:48 +0700

    deploy-commands.ts fully converted to ts without `//@ts-nocheck` or `any`

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,21 +1,22 @@
-//@ts-nocheck
 // Slash Commands Deployment Script
 // https://discordjs.guide/creating-your-bot/command-deployment.html#guild-commands/
 
 // Importing modules using ES6 syntax
 import { REST, Routes } from 'discord.js';
+import type { RESTPutAPIApplicationCommandsResult, RESTPutAPIApplicationGuildCommandsResult } from 'discord.js';
+import type { RESTPutAPIApplicationGuildCommandsJSONBody } from 'discord.js';
 import { config } from 'dotenv';
 import fs from 'node:fs';
 
 config(); // Using dotenv config function directly
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-(async () => {
-    for (const file of commandFiles) {
-    let i = 0; const c = [];
+async function main(token: string, clientId: string, serverId: string) {
+  const commands: RESTPutAPIApplicationGuildCommandsJSONBody[] = [];
+  const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+  
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    let i = 0; const c: string[] = [];
     const command = await import(`./commands/${file}`); // Using dynamic import
     let n = 1;
     while (true) {
@@ -36,7 +37,7 @@ const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith
   }
 
   // Construct and prepare an instance of the REST module
-  const rest = new REST().setToken(process.env.TOKEN);
+  const rest = new REST().setToken(token);
 
   // and deploy your commands!
   try {
@@ -46,17 +47,24 @@ const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith
 
     // In Discord terminology, "guild" and "server" are basically synonyms
     // So "SERVERID" from the .env file is "guildId" in the API
-    const guildData = await rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.SERVERID), {
+    const guildData: RESTPutAPIApplicationGuildCommandsResult = await rest.put(Routes.applicationGuildCommands(clientId, serverId), {
       // empty because we want to remove our old guild commands now that we've switched to global commands that work in DMs
-    });
+    }) as RESTPutAPIApplicationGuildCommandsResult;
 
-    const data = await rest.put(Routes.applicationCommands(process.env.CLIENTID), {
+    const data: RESTPutAPIApplicationCommandsResult = await rest.put(Routes.applicationCommands(clientId), {
         body: commands,
-    });
+    }) as RESTPutAPIApplicationCommandsResult;
 
     console.log(`Successfully reloaded ${guildData.length} guild application (/) commands, and ${data.length} global application (/) commands.`);
   } catch (error) {
     // And of course, make sure you catch and log any errors!
     console.error(error);
   }
-})();
+}
+
+if (process.env.TOKEN && process.env.CLIENTID && process.env.SERVERID) {
+  main(process.env.TOKEN, process.env.CLIENTID, process.env.SERVERID);
+} else {
+  const missingEnvVars = ['TOKEN', 'CLIENTID', 'SERVERID'].filter(v => !process.env[v]);
+  console.error(`Missing environment variables: ${missingEnvVars.join(', ')}`);
+}
