afd3ba169286a607ca8cfeedfeb44c4653456397
Andrew Dunbar <hippytrail@gmail.com>
2024-02-03 17:49:33 +0700

    fully migrate ago.ts to TypeScript without `//@ts-nocheck`

--- ute/ago.ts
+++ ute/ago.ts
@@ -1,5 +1,4 @@
-//@ts-nocheck
-export function ago(t) {
+export function ago(t: number) {
     const s = 1000;
     const m = s * 60;
     const h = m * 60;
@@ -9,7 +8,7 @@ export function ago(t) {
     const y = d * 365.25;
     const de = y * 10;
     const c = de * 20;
-    const x = [
+    const x: [number, string][] = [
         [c, 'century'],
         [de, 'decade'],
         [y, 'year'],
@@ -21,10 +20,10 @@ export function ago(t) {
         [s, 'second'],
     ];
 
-    let n;
-    var u;
-    let v;
-    let q;
+    let n: number;
+    var u: string = x[0][1];
+    let v: number = x[0][0];
+    let q: number;
     for (let i = 0; i < x.length; i++) {
         [v, u] = x[i];
         if (t >= v) break;
@@ -38,7 +37,7 @@ export function ago(t) {
         n === 1 && q === 0 ? '' : 's'
     } ago`;
 
-    function div(t, v) {
+    function div(t: number, v: number): [number, number] {
         const d = t/v;
         const i = Math.floor(d);
         return [i, Math.floor((d - i) * 4)];
