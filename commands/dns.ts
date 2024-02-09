f7cdafed8b10e0f8b32c1513fc299d9da1cc1f50
Andrew Dunbar <hippytrail@gmail.com>
2024-02-09 17:47:50 +0700

    better /dns logic after seeing how it works. now uses async/await

--- commands/dns.ts
+++ commands/dns.ts
@@ -1,38 +1,34 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import dns from 'dns';
+import { Resolver } from 'dns/promises';
 
 export const data = new SlashCommandBuilder()
     .setName('dns')
     .setDescription('Check if a domain is available')
     .addStringOption(option => option.setName('domain').setDescription('domain to check').setRequired(true));
 
-export const execute = dnsCheck;
+export const execute = dns;
 
-function checkDomainAvailability(domain: string, callback: (error: string | null, status: string) => void) {
-    dns.resolve(domain, (error) => {
-        if (error && error.code === 'ENOTFOUND') {
-            // Domain is not registered
-            callback(null, 'available');
-        } else if (error) {
-            // Error occurred during DNS resolution
-            callback(error.message, 'error');
-        } else {
-            // Domain is registered
-            callback(null, 'registered');
-        }
-    });
-}
-
-async function dnsCheck(interaction: ChatInputCommandInteraction) {
+async function dns(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
     const domain = interaction.options.getString('domain', true);
-    checkDomainAvailability(domain, async (error: string | null, status: string) => {
-        if (error) {
-            console.error('[DNS] An error occurred:', error);
-            await interaction.editReply('An error occurred while fetching data.');
+    try {
+        const resolver = new Resolver();
+        await resolver.resolve4(domain);
+        await interaction.editReply(`'${domain}': found it. so you can't register it.`);
+    } catch (error: any) {
+        console.error(error);
+        if (!['errno', 'code', 'syscall', 'hostname'].some(e => error[e]))
+            return await interaction.editReply(`'${domain}': missing expected fields in error.`);
+
+        if (error.errno !== undefined || error.syscall !== 'queryA')
+            return await interaction.editReply(`'${domain}': unexpected values in error.`);
+
+        if (error.code === 'ENOTFOUND') {
+            await interaction.editReply(`'${domain}': can't find it. so it might be available.`);
+        } else if (error.code === 'EBADNAME') {
+            await interaction.editReply(`'${domain}': don't be silly. that's not a proper domain name.`);
         } else {
-            console.log(`[DNS] Domain status: ${status}`);
-            await interaction.editReply(`Domain ${domain} is ${status}.`);
+            await interaction.editReply(`'${domain}': unexpected error code.`);
         }
-    });
+    }
 }
