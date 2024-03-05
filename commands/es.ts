531921d371a15c3b76a71bf1c822edb629cd39f8
Andrew Dunbar <hippytrail@gmail.com>
2024-03-05 21:48:10 +0700

    add english and spanish wiktionary checks to the /es command

--- commands/es.ts
+++ commands/es.ts
@@ -1,6 +1,7 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { Earl } from '../ute/earl';
 import { domStroll } from '../ute/dom';
+import { wikt } from '../ute/wikt';
 
 export const data = new SlashCommandBuilder()
     .setName('es')
@@ -12,12 +13,31 @@ export const execute = es;
 async function es(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
     try {
-        const word = interaction.options.getString('word')!;
-        console.log(`es: ${word}`);
+        const word = interaction.options.getString('word');
 
-        // https://dle.rae.es/WORD
-        const raeEarl = new Earl('https://dle.rae.es', word);
+        type DictTuple = [string, (word: string) => Promise<number | null>];
 
+        const replies = await Promise.all(([
+            ['rae', rae],
+            ['enwiktionary', () => wikt('en', word!)],
+            ['viwiktionary', () => wikt('es', word!)],
+        ] as Array<DictTuple>).map(async ([name, func]) => {
+            const reply = await func(word!);
+            return reply !== null
+                ? `${name}: ${reply} results.`
+                : `${name}: error.`
+        }));
+        
+        await interaction.editReply(replies.join('\n'));
+    } catch (error) {
+        console.error(error);
+        await interaction.editReply('An error occurred while fetching data.');
+    }
+}
+
+async function rae(word: string): Promise<number | null> {
+    const raeEarl = new Earl('https://dle.rae.es', word);
+    try {
         const resultados = domStroll('rae', false, await raeEarl.fetchDom(), [
             [2, 'html'],
             [3, 'body'],
@@ -28,8 +48,6 @@ async function es(interaction: ChatInputCommandInteraction) {
             [3, 'div', { id: 'resultados' }],
         ])!;
 
-        // console.log(resultados.children?.map((e,i) => `${i}\t${e.type}\t<${e.name??''}>\t#${e.attribs?.id??''}\t.${e.attribs?.class??''}`).join('\n'));
-
         const kidsTags = resultados.children!.filter(e => e.type === 'tag').map(e => e.name);
         const firstN = kidsTags.slice(0, -2);
         const last2 = kidsTags.slice(-2);
@@ -37,31 +55,27 @@ async function es(interaction: ChatInputCommandInteraction) {
         console.log(`[ES] first ${firstN.length}`, firstN); console.log(`[ES] last 2`, last2);
 
         if (['p', 'div'].some((e,i) => last2[i] !== e)) {
-            await interaction.editReply(`unexpected last 2 child nodes: ${last2.join(' ')}`);
-            return;
+            console.log('hmm...');
+            return null;
         }
 
         // Aviso: La palabra teju no está en el Diccionario. Las entradas que se muestran a continuación podrían estar relacionadas:
         if (['span', 'p', 'div'].every((e,i) => firstN[i] === e)) {
-            await interaction.editReply(`'${word}' not found`);
-            return;
+            return 0;
         }
 
         // Entradas que contienen la forma «tejas»:
         if (['div'].every((e,i) => firstN[i] === e)) {
-            await interaction.editReply(`'${word}' not found`);
-            return;
+            return 0;
         }
 
         if (firstN[0] === 'article') {
-            await interaction.editReply(`'${word}' found`);
-            return;
+            return 1;
         }
-        
-        await interaction.editReply('hmm...');
+        return null;
 
     } catch (error) {
         console.error(error);
-        await interaction.editReply('An error occurred while fetching data.');
+        return null;
     }
 }
