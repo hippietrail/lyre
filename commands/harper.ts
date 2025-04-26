e7baa6586fe0241200291fda10cd38ea721bbeb8
hippietrail <hippytrail@gmail.com>
2025-04-26 15:00:02 +0700

    refactor: old format behind `--old` switch, new format default

--- commands/harper.ts
+++ commands/harper.ts
@@ -1,5 +1,4 @@
 import { type ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-// TODO: use harperjs is it import or require and inside { these } or not?
 
 export const data = new SlashCommandBuilder()
     .setName('lint')
@@ -20,9 +19,44 @@ async function lint(interaction: ChatInputCommandInteraction) {
 
     const linter = new hjs.LocalLinter({ binary: hjs.binary });
 
-    const lints = await linter.lint(text);
+    // Check for experimental command: --option rest of the text
+    const match = text.match(/^--(\w+)\s+([\s\S]*)/);
+    if (match) {
+        const [_, command, rest] = match;
+        if (command === 'old') {
+            const lintyText = await linter.lint(rest)
+            .then(lints => lints
+                .map(lint => lint.suggestions()
+                    .map(s => {
+                        const [[prob_txt, prob_is_ws], [repl_txt, repl_is_ws]] = [lint.get_problem_text(), s.get_replacement_text()].map(s => {
+                            const is_ws = /^\s+$/.test(s);
+                            const maybe_quoted = is_ws ? `"${s}"` : s;
+                            return [maybe_quoted, is_ws];
+                        });
+                        return `${prob_txt} → ${repl_txt}${prob_is_ws || repl_is_ws ? ` ${lint.message()}` : ''}`;
+                    })
+                    .join('\n') || `${lint.get_problem_text()} → ?`)
+                .join('\n')
+            );
+            return await interaction.editReply(lintyText || '👍');
+        }
+        return await interaction.editReply('😕');
+    }
 
-    const lintText = lints.map(lint => lint.message()).join('\n');
-  
-    await interaction.editReply(lintText);
+    const lintyText = await linter.lint(text)
+    .then(lints => lints
+        .map(lint => `${lint.lint_kind()}: ${lint.get_problem_text()} → ${lint.suggestion_count() === 0 ? '?' : lint.suggestions()
+            .map(s => {
+                const [[repl_txt, repl_is_ws]] = [s.get_replacement_text()].map(s => {
+                    const is_ws = /^\s+$/.test(s);
+                    const maybe_quoted = is_ws ? `"${s}"` : s;
+                    return [maybe_quoted, is_ws];
+                });
+                return `${repl_txt}${repl_is_ws ? ` ${lint.message()}` : ''}`;
+            })
+            .join(' | ')}`)
+        .join('\n')
+    );
+
+    return await interaction.editReply(lintyText || '👍');
 }
